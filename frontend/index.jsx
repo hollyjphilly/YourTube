7280d2a92ec922b57cf7e239160455f384aabc98
hollyjphilly <61254925+hollyjphilly@users.noreply.github.com>
2021-03-08 18:36:07 -0500

    initial push of Rails project skeleton

---
+++ frontend/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Top from './top_component';
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
+  ReactDOM.render(<Top />, root);
+});
