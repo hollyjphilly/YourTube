c2730babfebfdef5674a99fcefe3ad772c93f081
hollyjphilly <61254925+hollyjphilly@users.noreply.github.com>
2021-03-15 17:32:35 -0400

    Prepare for initial Heroku push

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -6,7 +6,7 @@ import * as AJAX from './util/session_api_util';
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
   window.createUser = AJAX.createAccountAJAX;
-  window.removeUser = AJAX.removeAccountAJAX;
+  window.deleteUser = AJAX.removeAccountAJAX;
   window.editUser = AJAX.editAccountAJAX;
   window.login = AJAX.loginAJAX;
   window.logout = AJAX.logoutAJAX;
