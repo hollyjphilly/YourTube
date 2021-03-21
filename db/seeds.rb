cbb0195012138c556c8fb5d4a89af3195c7e4ee8
hollyjphilly <61254925+hollyjphilly@users.noreply.github.com>
2021-03-21 00:47:21 -0400

    take out comma for heroku in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,17 +11,26 @@ require 'open-uri'
 User.destroy_all
 ApplicationRecord.connection.reset_pk_sequence!('users')
 Video.destroy_all
-ApplicationRecord.connection.reset_pk_sequence!('video')
+ApplicationRecord.connection.reset_pk_sequence!('videos')
 User.create(username: 'demo', password: 'password', email:'jane@demoemail.com', first_name: 'Jane', last_name: 'McDemo', profile_image_url: 'https://i.ibb.co/x8170Cw/1516927665677-e-2159024400-v-beta-t-Rx-N5n-z8rd8da6-Wvi-LYt-F5-Ke-Tvv-Jib-FL1sw-NI45-QTo-U.jpg')
 User.create(username: 'banana', password: 'password', email:'banana@peel.com', first_name: 'bana', last_name: 'na', profile_image_url: 'https://i.ibb.co/XDTygG1/image.png')
 User.create(username: 'dinosaur', password: 'asteroid', email:'tee@rex.com', first_name: 'dino', last_name: 'saurus')
+
 vid1 = Video.create!(title: "Party Time", description: "This is what it feels like when you pass all aA assessments", uploader_id: 1)
-vid1.moviefile.attach(io: "https://yourtube-seeds.s3.amazonaws.com/partytime.mov"), filename: "partytime.mov")
-vid1.thumbnail.attach(io: "https://yourtube-seeds.s3.amazonaws.com/partythumb.png"), filename: "partythumb.png")
+file = open("https://yourtube-seeds.s3.amazonaws.com/partytime.mov")
+vid1.moviefile.attach(io: file)
+file = open("https://yourtube-seeds.s3.amazonaws.com/partythumb.png")
+vid1.thumbnail.attach(io: file)
+
 vid2 = Video.create!(title: "Let's Test Video", description: "Follow me on @bughunter on instagram", uploader_id: 2)
-vid2.moviefile.attach(io: open("https://yourtube-seeds.s3.amazonaws.com/csslife.mov"), filename: "csslife.mov")
-vid2.thumbnail.attach(io: open("https://yourtube-seeds.s3.amazonaws.com/cssthumb.png"), filename: "cssthumb.png")
+file = open("https://yourtube-seeds.s3.amazonaws.com/csslife.mov")
+vid2.moviefile.attach(io: file)
+file = open("https://yourtube-seeds.s3.amazonaws.com/cssthumb.png")
+vid2.thumbnail.attach(io: file)
+
 vid3 = Video.create!(title: "Cans in a Bag", description: "Getcha head in the game, or get a can in a bag", uploader_id: 3)
-vid3.moviefile.attach(io: open("https://yourtube-seeds.s3.amazonaws.com/cansinbag.mp4"), filename: "cansinbag.mp4")
-vid3.thumbnail.attach(io: open("https://yourtube-seeds.s3.amazonaws.com/cssthumb.png"), filename: "cssthumb.png")
+file = open("https://yourtube-seeds.s3.amazonaws.com/cansinbag.mp4")
+vid3.moviefile.attach(io: file)
+file = open("https://yourtube-seeds.s3.amazonaws.com/cssthumb.png")
+vid3.thumbnail.attach(io: file)
 
